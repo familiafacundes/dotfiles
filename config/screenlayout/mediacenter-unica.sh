6c7125fa72d2db2ee8a7704f02a27c99fd3425fd
familia <61797304+familiafacundes@users.noreply.github.com>
2020-03-06 00:29:45 -0300

    Update mediacenter-unica.sh

--- config/screenlayout/mediacenter-unica.sh
+++ config/screenlayout/mediacenter-unica.sh
@@ -4,3 +4,4 @@ xrandr --newmode "1360x768_60.00" 60.00 1360 1432 1568 1776 768 771 781 798 -hsy
 xrandr --addmode LVDS1 1360x768_60.00
 xrandr|grep 1360
 xrandr --output LVDS1 --mode 1360x768_60.00 --pos 0x0 --output HDMI1 --mode 1360x768 --pos 0x0
+xrandr --output HDMI1 --mode 1360x768 --pos 0x0 --output LVDS1 --mode 1360x768_60.00 --pos 0x0
